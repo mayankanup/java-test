5ca92dce0406b1bdf4992c8b4f9d8100ba31d4b9
mayankanup <anup.mayank@gmail.com>
2025-08-06 22:39:18 +0530

    Modularized into Hidato objects

--- demo/src/main/java/com/example/HidatoSolver.java
+++ demo/src/main/java/com/example/HidatoSolver.java
@@ -1,6 +1,8 @@
 package com.example;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class HidatoSolver {
@@ -8,6 +10,45 @@ public class HidatoSolver {
     int BLOCKED = -1;
 
     public void test(){
+        List<Hidato> boards = new ArrayList<Hidato>();
+        boards.add(getBoard2());
+        boards.add(getBoard3());
+        //boards.add(getBoard1());
+        for (Hidato hidato: boards){
+            //System.out.println(validate(board, start, end));
+            long t1 = System.currentTimeMillis();
+            boolean solutionExists = solve(hidato, 0, 0, getFilledValues(hidato.board));
+            long t2 = System.currentTimeMillis();
+            System.out.println("Found solution :" + solutionExists);
+            System.out.println("Time taken in ms = " + (t2-t1));
+            printBoard(hidato.board);
+        }
+        
+    }
+
+    private void printBoard(int [][] board){
+        for(int i=0; i< board.length; i++){
+            for (int j=0; j< board.length; j++){
+                System.out.print(board[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
+    private Set<Integer> getFilledValues(int [][] board){
+        //Keep all filled numbers in a set
+        Set<Integer> filledValues = new HashSet<Integer>();
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board.length; j++){
+                int val = board[i][j];
+                if(val != EMPTY && val != BLOCKED){
+                    filledValues.add(board[i][j]);
+                }
+            }
+        }
+        return filledValues;
+    }
+
+    private Hidato getBoard1(){
         int [][] board = {
             {-1,30,28,24,0,0,0,0,-1},
             {0,31,0,0,0,0,61,59,58},
@@ -21,14 +62,15 @@ public class HidatoSolver {
         };
         int start = 1;
         int end = 77;
+        Hidato hidato = new Hidato();
+        hidato.board = board;
+        hidato.start = start;
+        hidato.end = end;
 
-        /*int [][] board = {
-            {5,0,2},
-            {0,1,0},
-            {7,0,9}
-        };
-        int start = 1;
-        int end = 9;
+        return hidato;
+    }
+
+    private Hidato getBoard2() {
         int [][] board = {
             {13,0,15,0,0},
             {0,25,1,2,18},
@@ -37,35 +79,38 @@ public class HidatoSolver {
             {9,0,0,6,0}
         };
         int start = 1;
-        int end = 25;*/
+        int end = 25;
+        Hidato hidato = new Hidato();
+        hidato.board = board;
+        hidato.start = start;
+        hidato.end = end;
 
-        //System.out.println(validate(board, start, end));
-        long t1 = System.currentTimeMillis();
-        boolean solutionExists = solve(board, 0, 0, start, end, getFilledValues(board));
-        long t2 = System.currentTimeMillis();
-        System.out.println("Found solution :" + solutionExists);
-        System.out.println("Time taken in ms = " + (t2-t1));
-        for(int i=0; i< board.length; i++){
-            for (int j=0; j< board.length; j++){
-                System.out.print(board[i][j]+ " ");
-            }
-            System.out.println();
-        }
+        return hidato;
     }
-    private Set<Integer> getFilledValues(int [][] hidato){
-        //Keep all filled numbers in a set
-        Set<Integer> filledValues = new HashSet<Integer>();
-        for(int i=0; i<hidato.length; i++){
-            for(int j=0; j<hidato.length; j++){
-                int val = hidato[i][j];
-                if(val != EMPTY && val != BLOCKED){
-                    filledValues.add(hidato[i][j]);
-                }
-            }
-        }
-        return filledValues;
+
+    private Hidato getBoard3() {
+        int [][] board = {
+            {5,0,2},
+            {0,1,0},
+            {7,0,9}
+        };
+        int start = 1;
+        int end = 9;
+        Hidato hidato = new Hidato();
+        hidato.board = board;
+        hidato.start = start;
+        hidato.end = end;
+
+        return hidato;
     }
-    private boolean solve(int[][] board, int row, int col, int start, int end, Set<Integer> filledValues) {
+    
+
+    class Hidato{
+        int [][] board;
+        int start, end;
+    }
+    private boolean solve(Hidato hidato, int row, int col, Set<Integer> filledValues) {
+        int [][] board = hidato.board;
         // Base case: If row is equal to board length, entire board has been filled
         if (row == board.length) {
             return true;
@@ -74,22 +119,22 @@ public class HidatoSolver {
         // Move to next row when current row is fully filled
         if (col == board.length) {
             //System.out.println("Filled row : " + row);
-            return solve(board, row + 1, 0, start, end, filledValues);
+            return solve(hidato, row + 1, 0, filledValues);
         }
         
         // Skip cells that are already filled
         if (board[row][col] != EMPTY && board[row][col] != BLOCKED) {
-            return solve(board, row, col + 1, start, end, filledValues);
+            return solve(hidato, row, col + 1, filledValues);
         }
         
         // Try different numbers in current cell
-        for (int num = start; num <= end; num++) {
+        for (int num = hidato.start; num <= hidato.end; num++) {
             if(filledValues.contains(num)) continue;
-            if (isValidPlacement(board, row, col, num, start, end, filledValues)) {
+            if (isValidPlacement(hidato, row, col, num, filledValues)) {
                 board[row][col] = num; // Fill current cell with valid number
                 filledValues.add(num);
                 // Move to next cell
-                if (solve(board, row, col + 1, start, end, filledValues)) {
+                if (solve(hidato, row, col + 1, filledValues)) {
                     return true;
                 }
                 
@@ -103,28 +148,30 @@ public class HidatoSolver {
         return false;
     }
 
-    private boolean isValidPlacement(int[][] board, int row, int col, int num, int start, int end, Set<Integer> filledValues) {
-        board[row][col] = num;
-        boolean isValid = validate(board, start, end, filledValues);
-        board[row][col] = EMPTY;
+    private boolean isValidPlacement(Hidato hidato, int row, int col, int num, Set<Integer> filledValues) {
+        hidato.board[row][col] = num;
+        boolean isValid = validate(hidato, filledValues);
+        hidato.board[row][col] = EMPTY;
         return isValid;
     }
 
-    private boolean validate(int [][] hidato, int start, int end, Set<Integer> filledValues){
-        
-        for(int i=0; i<hidato.length; i++){
-            for(int j=0; j<hidato.length; j++){
-                int val = hidato[i][j];
+    private boolean validate(Hidato hidato, Set<Integer> filledValues){
+        int [][] board = hidato.board;
+        int start = hidato.start;
+        int end = hidato.end;
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board.length; j++){
+                int val = board[i][j];
                 if(val != EMPTY && val != BLOCKED){
                     int previous = val - 1;
                     if(previous>=start && filledValues.contains(previous)){
-                        if(!isNeighbor(previous, i, j, hidato)){
+                        if(!isNeighbor(previous, i, j, board)){
                             return false;
                         }
                     }
                     int next = val + 1;
                     if(next<=end && filledValues.contains(next)){
-                        if(!isNeighbor(next, i, j, hidato)){
+                        if(!isNeighbor(next, i, j, board)){
                             return false;
                         }
                     }
